834eca46da3374c2cc3370a981b375ef5d003279
alanymara <alanymara@gmail.com>
2022-01-06 16:56:51 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species
+    .find((item1) => item1.name === animal).residents
+    .every((item2) => item2.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
