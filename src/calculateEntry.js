32972d214a5b52af2d68252b41c70002b08c6c53
alanymara <alanymara@gmail.com>
2022-01-07 20:46:29 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-
+  const child = entrants.filter(({ age }) => age < 18);
+  const adult = entrants.filter(({ age }) => age >= 18 && age < 50);
+  const senior = entrants.filter(({ age }) => age >= 50);
+  return { adult: adult.length, child: child.length, senior: senior.length };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!Array.isArray(entrants)) return 0;
+
+  const { adult, child, senior } = prices;
+  const visitantes = countEntrants(entrants);
+  const { adult: adultos, child: criancas, senior: idosos } = visitantes;
+
+  const totalPrice = (adult * adultos) + (child * criancas) + (senior * idosos);
+  return totalPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
