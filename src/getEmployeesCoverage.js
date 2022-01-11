783e2a889237c79cfa8a6b2d07b64366d5990f87
alanymara <alanymara@gmail.com>
2022-01-11 14:19:05 -0300

    Requisito 10 - Mensagem padrão para função chamada sem parâmetro

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,23 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getInformations(responsavelPor, propriedade) {
+  return species.filter(({ id }) => responsavelPor
+    .includes(id)).map((animal) => animal[propriedade]);
 }
 
+function getEmployeesCoverage(personIdentification) {
+  const objectEmployees = employees.map(({ id, responsibleFor, firstName, lastName }) => {
+    const object = { id,
+      fullName: `${firstName} ${lastName}`,
+      species: getInformations(responsibleFor, 'name'),
+      locations: getInformations(responsibleFor, 'location') };
+    return object;
+  });
+  /* if (objectEmployees.filter((employee) => employee.fullName
+    .includes(personIdentification))) {
+    return 
+  } */
+  return objectEmployees;
+}
+console.log(getEmployeesCoverage());
 module.exports = getEmployeesCoverage;
